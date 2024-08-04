654786bf875ff72b207dfc6c878879c1a1fd5b36
Thiago Lira <thi.lira01@hotmail.com>
2024-08-04 12:02:41 -0300

    teste de atualziação usando A IDE

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula01OlaDevDojo {
     public static void main (String[] args){
         System.out.println("Olá Mundo");
+        System.out.println("Esse é meu primeiro codigo em Java");
 
     }
 }
